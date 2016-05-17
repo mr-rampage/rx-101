ec8012b348710d87ce441e68eea52cfb7debfcb0
mr-rampage <readonly@che.wbac.ca>
2016-05-17 00:48:06 -0400

    Removing state on component

--- src/js/component/userName.js
+++ src/js/component/userName.js
@@ -2,21 +2,12 @@ import Rx from "rx-dom-events/rx.dom.events";
 import DOMFactory from "./dom-factory"
 
 function createComponent(name, value) {
-  let _userName;
-
   let template = `<input type="text" name="${name}" value="${value}"/>`;
   let element = DOMFactory(template);
 
   let observable = Rx.DOM.keyup(element)
     .pluck('target', 'value')
     .filter(text => text.length > 2);
-
-  let observer = Rx.Observer.create(
-    userName => _userName = userName,
-    err => _userName = ''
-  );
-
-  observable.subscribe(observer);
 
   return {
     stream: observable,
